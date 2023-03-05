31f9f5b916308c7b661511855cbd09322b71997e
fevelezf <fevelezf@unal.edu.co>
2023-03-05 16:27:43 -0500

    5

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -2,8 +2,8 @@ package classroom;
 
 public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
-Tipo(int i) {
+	private int codigo;
+	private Tipo(int codigo) {
+		this.codigo = codigo;
+	}
 }
-}
-
-
